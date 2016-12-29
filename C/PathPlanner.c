fecadeb5f3df490443eb22a5fe84adcb6241f44c
TuringsEgo <Arsen4l27>
2016-12-29 09:20:59 -0600

    fixed mem leaks created by lazy me

---
+++ C/PathPlanner.c
@@ -0,0 +1,309 @@
+/**
+ * pseudo code
+ *
+ * 1. Reduce input waypoints to only essential (direction changing) node points
+ * 2. Calculate how many total datapoints we need to satisfy the controller for "playback"
+ * 3. Simultaneously inject and smooth the path until we end up with a smooth path with required number
+ *    of datapoints, and which follows the waypoint path.
+ * numWayPoints - number of waypoints
+ * dimWayPoint - dimensions of the waypoints
+ * maxTtotalTimeime - the max time allowed completing the path
+ * timeStep - the frequency of the motor controller
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * This method calculates the optimal parameters for determining what amount of nodes to inject into the path
+ * to meet the time restraint. This approach uses an iterative process to inject and smooth, yielding more desirable
+ * results for the final smooth path.
+ * Big O: Constant Time
+ */
+int* injectionCounter2Steps(float numNodeOnlyPoints, float maxTimeToComplete, float timeStep)
+{
+    int first = 0;
+    int second = 0;
+    int third = 0;
+
+    float oldPointsTotal = 0;
+
+    int* ret = (int*)malloc(3*sizeof(int));
+
+    float totalPoints = maxTimeToComplete / timeStep;
+
+    if (totalPoints < 100)
+    {
+        float pointsFirst;
+        float pointsTotal;
+
+
+        for (int i = 4; i <= 6; i++) {
+            for (int j = 1; j <= 8; j++) {
+                pointsFirst = i * (numNodeOnlyPoints - 1) + numNodeOnlyPoints;
+                pointsTotal = (j * (pointsFirst - 1) + pointsFirst);
+
+                if (pointsTotal <= totalPoints && pointsTotal > oldPointsTotal)
+                {
+                    first = i;
+                    second = j;
+                    oldPointsTotal = pointsTotal;
+                }
+            }
+        }
+    } else {
+        float pointsFirst;
+        float pointsSecond;
+        float pointsTotal;
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 8; j++) {
+                for (int k = 1; k < 8; k++) {
+                    pointsFirst = i * (numNodeOnlyPoints - 1) + numNodeOnlyPoints;
+                    pointsSecond = (j * (pointsFirst - 1) + pointsFirst);
+                    pointsTotal = (k * (pointsSecond - 1) + pointsSecond);
+
+                    if (pointsTotal <= totalPoints) {
+                        first = i;
+                        second = j;
+                        third = k;
+                    }
+                }
+            }
+        }
+    }
+
+    ret[0] = first;
+    ret[1] = second;
+    ret[2] = third;
+    return ret;
+}
+
+/**
+ * Method upsamples the Path by linear injection. The result providing more waypoints along the path.
+ * BigO: Order N * injection#
+ */
+float** inject(int numWayPoints, int dimWayPoint, float** orig, int numToInject) {
+    //create extended n Dimensional array to hold additional points
+    int newNumWayPoints = numWayPoints + (numToInject * (numWayPoints - 1));
+    float** morePoints = malloc(newNumWayPoints * sizeof(float*));
+    for (int i = 0; i < newNumWayPoints; i++) {
+      morePoints[i] = malloc(dimWayPoint * sizeof(float));
+    }
+
+    int index = 0;
+
+    //loop through original array
+    for (int i = 0; i < numWayPoints - 1; i++) {
+
+        //copy first
+        for(int j = 0; j < dimWayPoint; j++) {
+            morePoints[index][j] = orig[i][j];
+        }
+
+        index++;
+
+        for (int j = 1; j < numToInject + 1; j++) {
+            for (int k = 0; k < dimWayPoint; k++) {
+                //calculate intermediate x points between j and j+1 original points
+                morePoints[index][k] = j * ((orig[i + 1][k] - orig[i][k]) / (numToInject + 1)) + orig[i][k];
+            }
+            index++;
+        }
+    }
+
+    //copy last
+    for(int j = 0; j < dimWayPoint; j++) {
+        morePoints[index][j] = orig[numWayPoints-1][j];
+    }
+
+    return morePoints;
+}
+
+/**
+ * Optimization algorithm, which optimizes the data points in path to create a smooth trajectory.
+ * This optimization uses gradient descent. While unlikely, it is possible for this algorithm to never
+ * converge. If this happens, try increasing the tolerance level.
+ * BigO: N^x, where X is the number of of times the while loop iterates before tolerance is met.
+ */
+float** smoother(int numWayPoints, int dimWayPoint, float** path, float weight_data, float weight_smooth, float tolerance) {
+    //copy array
+    float**  newPath = malloc(numWayPoints * sizeof(float*));
+    for (int i = 0; i < numWayPoints; i++) {
+        newPath[i] = malloc(dimWayPoint * sizeof(float));
+    }
+
+    for(int i = 0; i < numWayPoints; i++) {
+        for(int j = 0; j < dimWayPoint; j++) {
+            newPath[i][j] = path[i][j];
+        }
+    }
+
+    double change = tolerance;
+    while (change >= tolerance) {
+        change = 0.0;
+        for (int i = 1; i < numWayPoints - 1; i++) {
+            for (int j = 0; j < dimWayPoint; j++) {
+                double aux = newPath[i][j];
+                newPath[i][j] += weight_data * (path[i][j] - newPath[i][j]) + weight_smooth * (newPath[i - 1][j] + newPath[i + 1][j] - (2.0 * newPath[i][j]));
+                change += abs(aux - newPath[i][j]);
+            }
+        }
+    }
+
+    return newPath;
+}
+
+
+float** smoothPath(int numWayPoints, int dimWayPoint, float** wayPoints, float totalTime, float timeStep)
+{
+    //the path we are going to return
+    float** path;
+
+    //important values used for smoothing
+    float pathAlpha = 0.7;
+    float pathBeta = 0.3;
+    float pathTolerance = 0.0000001;
+
+    int numPoints = numWayPoints;
+    int old_num_points = numWayPoints;
+
+    int* injections = injectionCounter2Steps(numWayPoints, totalTime, timeStep);
+    //iteratively inject and smooth the path
+    for (int i = 0; i < 3; i++) {
+        if (i == 0) {
+            //tmp container for unsmoothed exented path. Yeah C...
+            float** tmp1 = inject(numPoints, dimWayPoint, wayPoints, injections[0]);
+
+            //update the number of points in our path before proceeding
+            numPoints = numPoints + ((injections[i]) * (numPoints- 1));
+
+            //Since path is an empty float**, there is no memory to free
+            path = smoother(numPoints, dimWayPoint, tmp1, pathAlpha, pathBeta, pathTolerance);
+
+            //clean up memory
+            for(int i = 0; i < numPoints; i++){
+                free(tmp1[i]);
+            }
+            free(tmp1);
+        } else {
+            //tmp container for unsmoothed exented path. Yeah C...
+            float** tmp1  = inject(numPoints, dimWayPoint, path, injections[i]);
+
+            //update the number of points in our path before proceeding
+
+            numPoints = numPoints + ((injections[i]) * (numPoints- 1));
+            //Another tmp container for smoothed exented path. Yeah C...
+            float** tmp2 = smoother(numPoints, dimWayPoint, tmp1, pathAlpha, pathBeta, pathTolerance);
+
+            //clean up memory
+            for(int i = 0; i < numPoints; i++){
+                free(tmp1[i]);
+            }
+            free(tmp1);
+
+            //free path
+            for(int i = 0; i < old_num_points; i++) {
+                free(path[i]);
+            }
+            free(path);
+
+            //re-allocate to the size of [numPonts][dimWayPoint]
+            path = malloc(numPoints * sizeof(float*));
+            for(int i = 0; i < numPoints; i++) {
+                path[i] = malloc(dimWayPoint* sizeof(float));
+            }
+
+            for(int i = 0; i < numPoints; i++) {
+                for(int j = 0; j < dimWayPoint; j++) {
+                    path[i][j] = tmp2[i][j];
+                }
+            }
+
+            //clean up memory
+            for(int i = 0; i < numPoints; i++){
+                free(tmp2[i]);
+            }
+            free(tmp2);
+
+        }
+        old_num_points = numPoints;
+    }
+
+    //clean up memory
+    free(injections);
+
+    //shift all the points down 1 and store the length in [0][0], including
+    //the added point.
+    float** finalPath = malloc((numPoints + 1) * sizeof(float*));
+    for(int i = 0; i < numPoints + 1; i++) {
+      finalPath[i] = malloc(dimWayPoint * sizeof(float));
+    }
+
+    finalPath[0][0] = numPoints + 1;
+
+    for(int i = 0; i < numPoints; i++) {
+        for(int j = 0; j < dimWayPoint; j++) {
+            finalPath[i+1][j] = path[i][j];
+        }
+    }
+
+    //clean up memory
+    for (int i = 0; i < numPoints; i++) {
+      free(path[i]);
+    }
+    free(path);
+
+    return finalPath;
+}
+
+void test()
+{
+    int num_points = 5;
+    int dim_points = 2;
+    float** points = malloc(num_points * sizeof(float*));
+    for (int i = 0; i < num_points; i++) {
+      points[i] = malloc(dim_points * sizeof(float));
+    }
+
+    points[0][0] = 1;
+    points[0][1] = 2;
+    points[1][0] = 2;
+    points[1][1] = 7;
+    points[2][0] = 4;
+    points[2][1] = 7;
+    points[3][0] = 6;
+    points[3][1] = 9;
+    points[4][0] = 10;
+    points[4][1] = 11;
+
+    float totalTime = 15;
+    float timeStep = .1;
+
+    float** sPath = smoothPath(num_points, dim_points, points, totalTime, timeStep);
+
+    for(int i = 1; i < sPath[0][0]; i++) {
+        for(int j = 0; j < dim_points; j++) {
+            //printf("%f ", sPath[i][j]);
+        }
+        //printf("\n");
+    }
+
+    //clean up memory
+    for (int i = 0; i < num_points; i++) {
+      free(points[i]);
+    }
+    free(points);
+
+    for (int i = 0; i < sPath[0][0]; i++) {
+      free(sPath[i]);
+    }
+    free(sPath);
+}
+
+int main()
+{
+    test();
+    return 0;
+}
---
+++ Java/PathPlanner.java
@@ -0,0 +1,290 @@
+/**
+ * This Class provides many useful algorithms for Robot Path Planning. It uses optimization techniques and knowledge
+ * of Robot Motion in order to calculate smooth path trajectories, if given only discrete waypoints. The Benefit of these optimization
+ * algorithms are very efficient path planning that can be used to Navigate in Real-time.
+ * <p>
+ * This Class uses a method of Gradient Decent, and other optimization techniques to produce a smooth path
+ * <p>
+ * This Class does not attempt to calculate quintic or cubic splines for best fitting a curve. It is for this reason, the algorithm can be ran
+ * on embedded devices with very quick computation times.
+ */
+public class PathPlanner {
+    //The waypoint only path
+    private static double[][] origPath;
+
+    //Tuninmg parameters for tuning the path generated
+    double pathAlpha;
+    double pathBeta;
+    double pathTolerance;
+
+    //The origPath with injected points to allow for smooth transitions
+    private double[][] smoothPath;
+
+    /**
+     * Constructor, takes a Path of Way Points defined as a double array of column vectors representing robot value
+     * such as x y coordinates, or an arm angle
+     * <p>
+     * For example: here is a properly formated waypoint array
+     * <p>
+     * double[][] waypointPath = new double[][]{
+     * {1, 1, 0},
+     * {5, 1, 45},
+     * {9, 12, 90},
+     * {12, 9, 135},
+     * {15,6, 180},
+     * {15, 4, 225}
+     * };
+     * This path goes from {1,1,0} -> {5,1,45} -> {9,12,90} -> {12,9, 135} -> {15,6,180} -> {15,4,225}
+     * The units of these coordinates are position units assumed by the user (i.e inch, foot, meters)
+     * The units do not have to match between elements.
+     *
+     * @param path
+     */
+    public PathPlanner(double[][] path) {
+        origPath = ArrayCopy(path);
+
+        //default values
+        pathAlpha = 0.7;
+        pathBeta = 0.3;
+        pathTolerance = 0.0000001;
+    }
+
+    /**
+     * Prints Cartesian Coordinates to the System Output as Column Vectors in the Form X	Y
+     *
+     * @param path
+     */
+    public void print(double[][] path) {
+        for(int i = 0; i < path.length; i++) {
+            for (int j = 0; j < path[i].length; j++) {
+                System.out.print(path[i][j] + "\t");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    /**
+     * Performs a deep copy of a 2 Dimensional Array looping thorough each element in the 2D array
+     * <p>
+     * BigO: Order N x M
+     *
+     * @param arr
+     * @return
+     */
+    public static double[][] ArrayCopy(double[][] arr) {
+        //size first dimension of array
+        double[][] temp = new double[arr.length][arr[0].length];
+
+        for (int i = 0; i < arr.length; i++) {
+            //Resize second dimension of array
+            temp[i] = new double[arr[i].length];
+
+            //Copy Contents
+            System.arraycopy(arr[i], 0, temp[i], 0, arr[i].length);
+        }
+
+        return temp;
+    }
+
+    //main program
+    public static void main(String[] args) {
+        //create n dimensional waypoint path
+        double[][] waypoints = new double[][]{
+                {1, 2},
+                {2, 7},
+                {4, 7},
+                {6, 9},
+                {10, 11}
+        };
+
+        double totalTime = 15; //seconds
+        double timeStep = .1; //period of control loop on Rio, seconds
+
+        final PathPlanner path = new PathPlanner(waypoints);
+
+        path.setPathAlpha(0.7);
+        path.setPathBeta(0.3);
+        path.setPathTolerance(0.0000001);
+        path.calculate(totalTime, timeStep);
+
+        path.print(path.smoothPath);
+    }
+
+    /**
+     * Method upsamples the Path by linear injection. The result providing more waypoints along the path.
+     * <p>
+     * BigO: Order N * injection#
+     *
+     * @param orig
+     * @param numToInject
+     * @return
+     */
+    public double[][] inject(double[][] orig, int numToInject) {
+        //create extended n Dimensional array to hold additional points
+        double[][] morePoints = new double[orig.length + ((numToInject) * (orig.length - 1))][orig[0].length];
+        System.out.println(morePoints.length);
+        int index = 0;
+
+        //loop through original array
+        for (int i = 0; i < orig.length - 1; i++) {
+            //copy first
+            System.arraycopy(orig[i], 0, morePoints[index], 0, orig[i].length);
+            index++;
+
+            for (int j = 1; j < numToInject + 1; j++) {
+                for (int k = 0; k < orig[i].length; k++) {
+                    //calculate intermediate x points between j and j+1 original points
+                    morePoints[index][k] = j * ((orig[i + 1][k] - orig[i][k]) / (numToInject + 1)) + orig[i][k];
+                }
+                index++;
+            }
+        }
+
+        //copy last
+        System.arraycopy(orig[orig.length - 1], 0, morePoints[index], 0, morePoints[index - 1].length);
+
+        return morePoints;
+    }
+
+    /**
+     * Optimization algorithm, which optimizes the data points in path to create a smooth trajectory.
+     * This optimization uses gradient descent. While unlikely, it is possible for this algorithm to never
+     * converge. If this happens, try increasing the tolerance level.
+     * <p>
+     * BigO: N^x, where X is the number of of times the while loop iterates before tolerance is met.
+     *
+     * @param path
+     * @param weight_data
+     * @param weight_smooth
+     * @param tolerance
+     * @return
+     */
+    public double[][] smoother(double[][] path, double weight_data, double weight_smooth, double tolerance) {
+        //copy array
+        double[][] newPath = ArrayCopy(path);
+
+        double change = tolerance;
+        while (change >= tolerance) {
+            change = 0.0;
+            for (int i = 1; i < path.length - 1; i++) {
+                for (int j = 0; j < path[i].length; j++) {
+                    double aux = newPath[i][j];
+                    newPath[i][j] += weight_data * (path[i][j] - newPath[i][j]) + weight_smooth * (newPath[i - 1][j] + newPath[i + 1][j] - (2.0 * newPath[i][j]));
+                    change += Math.abs(aux - newPath[i][j]);
+                }
+            }
+        }
+
+        return newPath;
+    }
+
+    /**
+     * This method calculates the optimal parameters for determining what amount of nodes to inject into the path
+     * to meet the time restraint. This approach uses an iterative process to inject and smooth, yielding more desirable
+     * results for the final smooth path.
+     * <p>
+     * Big O: Constant Time
+     *
+     * @param numNodeOnlyPoints
+     * @param maxTimeToComplete
+     * @param timeStep
+     */
+    public int[] injectionCounter2Steps(double numNodeOnlyPoints, double maxTimeToComplete, double timeStep) {
+        int first = 0;
+        int second = 0;
+        int third = 0;
+
+        double oldPointsTotal = 0;
+
+        int[] ret;
+
+        double totalPoints = maxTimeToComplete / timeStep;
+
+        if (totalPoints < 100) {
+            double pointsFirst;
+            double pointsTotal;
+
+
+            for (int i = 4; i <= 6; i++)
+                for (int j = 1; j <= 8; j++) {
+                    pointsFirst = i * (numNodeOnlyPoints - 1) + numNodeOnlyPoints;
+                    pointsTotal = (j * (pointsFirst - 1) + pointsFirst);
+
+                    if (pointsTotal <= totalPoints && pointsTotal > oldPointsTotal) {
+                        first = i;
+                        second = j;
+                        oldPointsTotal = pointsTotal;
+                    }
+                }
+
+            ret = new int[]{first, second, third};
+        } else {
+
+            double pointsFirst;
+            double pointsSecond;
+            double pointsTotal;
+
+            for (int i = 1; i <= 5; i++)
+                for (int j = 1; j <= 8; j++)
+                    for (int k = 1; k < 8; k++) {
+                        pointsFirst = i * (numNodeOnlyPoints - 1) + numNodeOnlyPoints;
+                        pointsSecond = (j * (pointsFirst - 1) + pointsFirst);
+                        pointsTotal = (k * (pointsSecond - 1) + pointsSecond);
+
+                        if (pointsTotal <= totalPoints) {
+                            first = i;
+                            second = j;
+                            third = k;
+                        }
+                    }
+
+            ret = new int[]{first, second, third};
+        }
+        return ret;
+    }
+
+    public void setPathAlpha(double alpha) {
+        pathAlpha = alpha;
+    }
+
+    public void setPathBeta(double beta) {
+        pathBeta = beta;
+    }
+
+    public void setPathTolerance(double tolerance) {
+        pathTolerance = tolerance;
+    }
+
+    /**
+     * This code will calculate a smooth path based on the program parameters. If the user doesn't set any parameters, the will use the defaults optimized for most cases. The results will be saved into the corresponding
+     * class members. The user can then access .smoothPath, .leftPath, .rightPath, .smoothCenter as needed
+     * <p>
+     *
+     * @param totalTime - time the user wishes to complete the path in seconds. (this is the maximum amount of time the robot is allowed to take to traverse the path.)
+     * @param timeStep  - the frequency at which the robot controller is running on the robot.
+     */
+    public void calculate(double totalTime, double timeStep) {
+        /**
+         * pseudo code
+         *
+         * 1. Reduce input waypoints to only essential (direction changing) node points
+         * 2. Calculate how many total datapoints we need to satisfy the controller for "playback"
+         * 3. Simultaneously inject and smooth the path until we end up with a smooth path with required number
+         *    of datapoints, and which follows the waypoint path.
+         */
+
+        //Figure out how many nodes to inject
+        int[] inject = injectionCounter2Steps(origPath.length, totalTime, timeStep);
+
+        //iteratively inject and smooth the path
+        for (int i = 0; i < inject.length; i++) {
+            if (i == 0) {
+                smoothPath = inject(origPath, inject[0]);
+                //smoothPath = smoother(smoothPath, pathAlpha, pathBeta, pathTolerance);
+            } else {
+                smoothPath = inject(smoothPath, inject[i]);
+                //smoothPath = smoother(smoothPath, pathAlpha, pathBeta, pathTolerance);
+            }
+        }
+    }
+}
